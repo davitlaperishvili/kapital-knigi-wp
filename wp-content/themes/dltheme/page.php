d73869d5bf22a75ea58b17e87f852c84d31415e2
dlaperishvili <davitlaperishvili@gmail.com>
2023-01-25 11:41:53 +0400

    games backend is updated

--- wp-content/themes/dltheme/page.php
+++ wp-content/themes/dltheme/page.php
@@ -3,6 +3,7 @@
 <?php
 $menu = get_field("site_menu", "option");
 $main_buttons = get_field("main_buttons", "option");
+$games = get_field("games", "option");
 ?>
 
 <main class="home_page">
@@ -13,152 +14,25 @@ $main_buttons = get_field("main_buttons", "option");
             class="slots"
             id="elements"
           >
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/7.png"
-              />
-              <span> PIN-UP Fruits </span>
-            </a>
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/8.jpg"
-              />
-              <span> Millionaire </span>
-            </a>
+              <?php foreach ($games['games_list'] as $game) {?>
+                <a
+                  href="<?php echo $game['game_url'] ?>"
+                  class="slot"
+                >
+                  <img
+                    class="image__slots__main"
+                    src="<?php echo $game['game_image']['url'] ?>"
+                  />
+                  <span> <?php echo $game['game_title'] ?> </span>
+                </a>
+              <?php }?>
 
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/4.jpg"
-              />
-              <span> Space Wars </span>
-            </a>
 
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/6.png"
-              />
-              <span> Ecuador Gold </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/3.jpg"
-              />
-              <span> 243 Crystal Fruits </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/5.jpg"
-              />
-              <span> Bonanza </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/1.png"
-              />
-              <span> Allways Hot Fruits </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/2.png"
-              />
-              <span> Big Panda </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/15.png"
-              />
-              <span>Artemis Medusa </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/16.png"
-              />
-              <span> Mustang Gold </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/17.png"
-              />
-              <span> Pirates Map </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/18.png"
-              />
-              <span> Fu Fortunes </span>
-            </a>
-
-            <a
-              href="#"
-              class="slot"
-            >
-              <img
-                class="image__slots__main"
-                src="<?php echo get_template_directory_uri(); ?>/images/19.png"
-              />
-              <span> Tractor Bean </span>
-            </a>
           </div>
           <a
-            href="#"
+            href="<?php echo $games['games_button']['url'] ?>"
             class="href"
-            ><div class="button">Перейти на сайт</div></a
+            ><div class="button"><?php echo $games['games_button']['text'] ?></div></a
           >
           <div class="promo"></div>
           <div class="page_content_text"><?php the_content()?></div>
